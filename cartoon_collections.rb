549d397782cf0a6b70edb800654dc824c0eda1cd
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-24 19:01:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element|
-    return "#{element.capitalize}!"
+  planeteer_calls.map { |element| element.capitalize + "!"}
   end
 end
 
