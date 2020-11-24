51e155a9707df59e4e6c11a597ce68ae1a52923a
Luke Thinnes <lukethinnes@yahoo.com>
2020-11-24 19:01:47 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |element| 
+  planeteer_calls.map do |element|
     element.capitalize + "!"
   end
 end
